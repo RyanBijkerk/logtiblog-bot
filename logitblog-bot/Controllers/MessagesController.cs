1c83c4dff13f9136fccb28884e42cb0685f9f80f
Ryan Bijkerk <ryan@logitblog.com>
2016-11-30 08:08:16 +0100

    Changed the systemMessage so it is working again

--- logitblog-bot/Controllers/MessagesController.cs
+++ logitblog-bot/Controllers/MessagesController.cs
@@ -31,12 +31,60 @@ namespace logitblog_bot
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
             ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-            var returnMessage = await ResolveMessage(activity);
-            var reply = activity.CreateReply(returnMessage);
-            await connector.Conversations.ReplyToActivityAsync(reply);
+
+
+            if (activity.Type != ActivityTypes.Message)
+            {
+                await HandleSystemMessage(activity);
+                
+            }
+            else
+            {
+                var returnMessage = await ResolveMessage(activity);
+                var reply = activity.CreateReply(returnMessage);
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            
             var response = Request.CreateResponse(HttpStatusCode.OK);
 
             return response;
         }
+
+
+        private async Task HandleSystemMessage(Activity message)
+        {
+            await Task.Run(() =>
+            {
+                if (message.Type == ActivityTypes.DeleteUserData)
+                {
+                    // Implement user deletion here
+                    // If we handle user deletion, return a real message
+                }
+                else if (message.Type == ActivityTypes.ConversationUpdate)
+                {
+                    // Handle conversation state changes, like members being added and removed
+                    // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                    // Not available in all channels
+                }
+                else if (message.Type == ActivityTypes.ContactRelationUpdate)
+                {
+                    // Handle add/remove from contact lists
+                    // Activity.From + Activity.Action represent what happened
+                }
+                else if (message.Type == ActivityTypes.Typing)
+                {
+                    // Handle knowing tha the user is typing
+                }
+                else if (message.Type == ActivityTypes.Ping)
+                {
+                    var messageString = "ping";
+                    message.Type = "ping";
+                    return message.CreateReply(messageString);
+                }
+
+                return null;
+            });
+        }
+
     }
 }
--- logitblog-bot/Services/MessageService.cs
+++ logitblog-bot/Services/MessageService.cs
@@ -72,10 +72,6 @@ namespace logitblog_bot.Services
                     }
                 }
             }
-            else
-            {
-                await HandleSystemMessage(activity);
-            }
 
             return messageString;
         }
@@ -152,42 +148,5 @@ namespace logitblog_bot.Services
                 return messageString;
             });
         }
-
-
-        public async Task HandleSystemMessage(Activity message)
-        {
-            await Task.Run(() =>
-            {
-                if (message.Type == ActivityTypes.DeleteUserData)
-                {
-                    // Implement user deletion here
-                    // If we handle user deletion, return a real message
-                }
-                else if (message.Type == ActivityTypes.ConversationUpdate)
-                {
-                    // Handle conversation state changes, like members being added and removed
-                    // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                    // Not available in all channels
-                }
-                else if (message.Type == ActivityTypes.ContactRelationUpdate)
-                {
-                    // Handle add/remove from contact lists
-                    // Activity.From + Activity.Action represent what happened
-                }
-                else if (message.Type == ActivityTypes.Typing)
-                {
-                    // Handle knowing tha the user is typing
-                }
-                else if (message.Type == ActivityTypes.Ping)
-                {
-                    var messageString = "ping";
-                    message.Type = "ping";
-                    return message.CreateReply(messageString);
-                }
-
-                return null;
-            });
-        }
-
     }
 }
